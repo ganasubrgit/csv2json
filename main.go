af74cdf15df30e9f060a428e36bae13b84d6ddbf
hifly <hifly@hiflys-MacBook-Pro.local>
2018-12-08 20:25:16 -0600

    file added

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var (
+	JSONFILE = "file.json"
+	CSVFILE  = "file.csv"
+)
+
+type Employee struct {
+	FirstName string
+	LastName  string
+	Age       int
+	Sex       string
+}
+
+//error handeling
+func check(err error, ext int) {
+	if err != nil && ext == 1 {
+		fmt.Println(err)
+		os.Exit(1)
+	} else if err != nil && ext == 0 {
+		fmt.Println(err)
+	}
+}
+
+func main() {
+	csvFile, err := os.Open(CSVFILE)
+	check(err, 0) //error check
+	defer csvFile.Close()
+	reader := csv.NewReader(csvFile)
+	reader.FieldsPerRecord = -1
+
+	csvData, err := reader.ReadAll()
+	check(err, 1) //error check
+	var emp Employee
+	var employees []Employee
+
+	for _, each := range csvData {
+		emp.FirstName = each[0]
+		emp.LastName = each[1]
+		emp.Age, _ = strconv.Atoi(each[2])
+		emp.Sex = each[3]
+		employees = append(employees, emp)
+	}
+
+	// Convert to JSON
+	jsonData, err := json.Marshal(employees)
+	check(err, 1) //error check
+
+	fmt.Println(string(jsonData))
+
+	jsonFile, err := os.Create(JSONFILE)
+	check(err, 0) //error check
+	defer jsonFile.Close()
+
+	jsonFile.Write(jsonData)
+	jsonFile.Close()
+}
