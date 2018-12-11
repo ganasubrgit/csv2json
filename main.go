f0a4092caf0b02cc3ce8f3cb04eb4c7d8ddd8d51
Ganapathy.Santhanam <Ganapathy.Santhanam@target.com>
2018-12-11 11:09:15 -0600

    updated arguments

--- main.go
+++ main.go
@@ -3,27 +3,35 @@ package main
 import (
 	"encoding/csv"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"os"
 )
 
 var (
-	JSONFILE = "file.json"
-	CSVFILE  = "file.csv"
+	CSVFILE  = flag.String("csvfile", "file.csv", "CSV file to convert")
+	JSONFILE = flag.String("jsonfile", "file.json", "JSON output file")
 )
 
 func main() {
-	csvFile, err := os.Open(CSVFILE)
+
+	flag.Parse()
+	args := os.Args
+	if len(args) <= 1 {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+	os.Remove(*JSONFILE)
+	csvFile, err := os.Open(*CSVFILE)
 	check(err, 0) //error check
 	defer csvFile.Close()
 	reader := csv.NewReader(csvFile)
 	reader.FieldsPerRecord = -1
-
+	reader.TrimLeadingSpace = true
 	csvData, err := reader.ReadAll()
 	check(err, 1) //error check
-	hvalue := header(CSVFILE)
+	hvalue := header(*CSVFILE)
 	JSONKEY := map[string]string{}
-
 	for _, each := range csvData {
 		for i := 0; i < len(hvalue); i++ {
 			JSONKEY[hvalue[i]] = each[i]
@@ -33,11 +41,11 @@ func main() {
 }
 
 func printjson(m map[string]string) {
-	// Convert to JSON
+	//Convert to JSON
 	jsonData, err := json.Marshal(m)
 	check(err, 1) //error check
 	fmt.Println(string(jsonData))
-	jsonFile, err := os.OpenFile(JSONFILE, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	jsonFile, err := os.OpenFile(*JSONFILE, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	check(err, 0) //error check
 	defer jsonFile.Close()
 	jsonFile.Write(jsonData)
