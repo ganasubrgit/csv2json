ef8f66e7e6a20a1eddf1bfc5900dad2dc14bf021
Ganapathy.Santhanam <Ganapathy.Santhanam@target.com>
2018-12-13 13:17:25 -0600

    performance fix

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/csv"
 	"encoding/json"
 	"flag"
@@ -11,6 +12,7 @@ import (
 var (
 	CSVFILE  = flag.String("csvfile", "file.csv", "CSV file to convert")
 	JSONFILE = flag.String("jsonfile", "file.json", "JSON output file")
+	b        bytes.Buffer
 )
 
 func main() {
@@ -38,17 +40,16 @@ func main() {
 		}
 		printjson(JSONKEY)
 	}
+	fmt.Printf(b.String()) //print buffer collection
+	jsonFile, err := os.OpenFile(*JSONFILE, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	check(err, 0) //error check
+	defer jsonFile.Close()
+	jsonFile.Write(b.Bytes())
 }
 
 func printjson(m map[string]string) {
 	//Convert to JSON
 	jsonData, err := json.Marshal(m)
-	check(err, 1) //error check
-	fmt.Println(string(jsonData))
-	jsonFile, err := os.OpenFile(*JSONFILE, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-	check(err, 0) //error check
-	defer jsonFile.Close()
-	jsonFile.Write(jsonData)
-	jsonFile.Close()
-
+	check(err, 1)                   //error check
+	b.WriteString(string(jsonData)) //write to buffer
 }
